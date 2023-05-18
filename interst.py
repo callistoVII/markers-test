43c69d8a22c696953e146c5a1de004765ef0ebfd
callistovii <21545332-callistovii@users.noreply.replit.com>
2023-05-18 01:10:17 +0000

    Initial commit

---
+++ interst.py
@@ -0,0 +1,2 @@
+message = int(input("Using whole numbers, and no special characters, please enter the interest rate: "))
+print(message) 
---
+++ main.py
@@ -0,0 +1,25 @@
+print("Welcome to My Program!")
+name = input("what is your Company Name? ")
+print("Hello " + name + "!") 
+continue_order = True
+total_cost = 0
+
+while continue_order:
+  number_of_markers = int(input("How Many Copic Markers would you like to buy:  "))
+  if (number_of_markers < 10):
+    total_cost = number_of_markers * 6.99
+  elif (number_of_markers >= 10 and number_of_markers < 20):
+    total_cost = number_of_markers * 5.99
+  elif (number_of_markers >= 20 and number_of_markers < 35):
+    total_cost = number_of_markers * 4.99
+  elif (number_of_markers >= 35):
+    total_cost = number_of_markers * 3.99
+    number_of_markers = float(total_cost)
+    continue_order = False
+    
+  print( name + ", the total cost is: $" + str(total_cost))
+  another_order = input("Would you like to place another order: Yes or No? ")
+  if (another_order.lower() == "yes"):
+    continue_order = True
+  else:
+    print("Thank you for using this Program, " + name +", enjoy your Markers!")
